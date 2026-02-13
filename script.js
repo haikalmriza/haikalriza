e4a007ba3ccd8e58791b64972e3d90f76fe5b1a1
haikalmriza <haikalmriza@gmail.com>
2026-02-13 16:11:11 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+document.querySelectorAll(".card").forEach(card => {
+  observer.observe(card);
+});
